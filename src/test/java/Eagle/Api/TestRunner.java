89136daadb8b0b021abba2e96006c458ce9e4562
Adel <kramatullahadel48@gmail.com>
2021-11-26 22:47:25 -0500

    my first git commit

---
+++ src/test/java/Eagle/Api/TestRunner.java
@@ -0,0 +1,12 @@
+package Eagle.Api;
+
+import com.intuit.karate.junit5.Karate;
+
+class TestRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("API").relativeTo(getClass());
+    }    
+
+}
